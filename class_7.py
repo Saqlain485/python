7bc46b2db743bb09daf9581474025426f125be79
Saqlain485 <70142043@student.uol.edu.pk>
2024-10-29 09:22:25 -0700

    Update class_7.py

--- class_7.py
+++ class_7.py
@@ -1,18 +1,18 @@
-# def get_word(sentence,n):
-#     if n>0:
-#         words=sentence.split(" ")
-#         if len (words) >n:
-#             return words[n]
-#  print (get_word ("this is lesson about list",2))
-#         # print (get_word ("this is lesson about list",-4))
-#         # print (get_word ("this is lesson about list",1))
-#         # print (get_word ("this is lesson about list",5))
-# fruits=["pineapple","banana","apple","melon"]
-# fruits.append("kiwi")
-# print(fruits)
-# def convert_seconds(seconds):
-#     hours=seconds//3600
-#     minutes=(seconds-hours*3600)
+def get_word(sentence,n):
+    if n>0:
+        words=sentence.split(" ")
+        if len (words) >n:
+            return words[n]
+ print (get_word ("this is lesson about list",2))
+        # print (get_word ("this is lesson about list",-4))
+        # print (get_word ("this is lesson about list",1))
+        # print (get_word ("this is lesson about list",5))
+fruits=["pineapple","banana","apple","melon"]
+fruits.append("kiwi")
+print(fruits)
+def convert_seconds(seconds):
+    hours=seconds//3600
+    minutes=(seconds-hours*3600)
 a=["eat","sleep","repeat"]
 b=["walk"]
 obj1=enumerate (a)
