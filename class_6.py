44f46931dfe635f717e8189381a9d3f8717a4fe1
Saqlain485 <70142043@student.uol.edu.pk>
2024-10-29 09:22:55 -0700

    Update class_6.py

--- class_6.py
+++ class_6.py
@@ -1,34 +1,34 @@
 text = "this is my string write your own string we done sliceing write now"
 text2="12345"
-# print(text[0:6])
-# print(text[6:])
-# text [2]="e"
-# string is immutable
-# print ( text.index("is"))
-# print ("write" in text)
-# print (text.upper())
-# print (text.lower())
-# answer = 'yes'
-# if answer.lower()== "yes":
-#     print ("user said yes")
-# answer = 'yes'
-# if answer.strip()== "yes":
-#     print ("user said yes")
-# text.count("is")
-# print (text.count("is"))
+print(text[0:6])
+print(text[6:])
+text [2]="e"
+string is immutable
+print ( text.index("is"))
+print ("write" in text)
+print (text.upper())
+print (text.lower())
+answer = 'yes'
+if answer.lower()== "yes":
+    print ("user said yes")
+answer = 'yes'
+if answer.strip()== "yes":
+    print ("user said yes")
+text.count("is")
+print (text.count("is"))
 
-# print (text.isalnum())
-# print (text + "-"+text2)
-# print("-".join([text,text2]))
-# print(text.split())
-# name="saqlain"
-# number=len(name)*3
-# print("hello {},you have won{} ,pounds and your name is{}".format(name,number,name))
-# price=7.5
-# with_tax=price*1.09
-# print(price,with_tax)
-# print("base price:${:.2f}.with tax:${:.2f}.format(price,withtax)")
-# def to_celsius(x):
-#     return (x-32)*5/9
-# for x in range(0,101,10):
-#     print("{:>3}F |{:>6.2f}C".format(x,to_celsius(x)))
+print (text.isalnum())
+print (text + "-"+text2)
+print("-".join([text,text2]))
+print(text.split())
+name="saqlain"
+number=len(name)*3
+print("hello {},you have won{} ,pounds and your name is{}".format(name,number,name))
+price=7.5
+with_tax=price*1.09
+print(price,with_tax)
+print("base price:${:.2f}.with tax:${:.2f}.format(price,withtax)")
+def to_celsius(x):
+    return (x-32)*5/9
+for x in range(0,101,10):
+    print("{:>3}F |{:>6.2f}C".format(x,to_celsius(x)))
